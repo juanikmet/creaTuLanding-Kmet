716b60855b9f87ca3de59e095cfbb899f3f96bf9
Juani Kmet <juancruz.kmet@gmail.com>
2024-07-18 22:48:11 -0300

    Initial commit

---
+++ Entrega React/src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+const ItemListContainer = ({greeting}) => {
+    return(
+        
+        <div className="bg-blue-400 w-full h-screen">
+            <h1 className="text-4xl text-white ">{greeting}</h1>
+        </div>
+    )
+};
+
+export default ItemListContainer;
